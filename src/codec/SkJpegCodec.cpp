f7632a6635af4b71d5d7f4deee2f90bf121b027b
Mort (Tianqi) Guo <tianqig@codeaurora.org>
2018-05-18 15:58:44 -0700

    Fix ABI Compliance Issue for JPEG Merged Upsample
    Merged upsampler of libjpeg-turbo causes crashes for certain image
    formats in regional decoding. To address this, a new field was added
    to jpeg_decompress_struct. The change in structure leads to ABI
    non-complaince. Fixing the same.
    
    CRs-Fixed: 2245074
    
    Change-Id: I451f8b7d0493d87e47f7a3a4808b8b650ecaf74c
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- src/codec/SkJpegCodec.cpp
+++ src/codec/SkJpegCodec.cpp
@@ -651,7 +651,6 @@ void SkJpegCodec::setupJpegDecoding(jpeg_decompress_struct* dinfo)
     }
 
     dinfo->do_fancy_upsampling = FALSE;
-    dinfo->disable_merged_upsampling = TRUE;
     dinfo->dct_method = JDCT_IFAST;
 }
 
